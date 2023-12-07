f265289f383b1a4dc3d5ae4bdaff5edbe8fcecbd
EmreSimseker <112547729+EmreSimseker@users.noreply.github.com>
2023-12-07 20:15:36 +0100

    Update reg.php

--- inlogPaginaPHP/reg.php
+++ inlogPaginaPHP/reg.php
@@ -1,29 +1,77 @@
+<?php
+session_start();
+
+if (isset($_SESSION['email'])) {
+    $email = $_SESSION['email'];
+    
+    $host = "localhost";
+    $dbname = "inlogproject_es";
+    $username = "root";
+    $password = "";
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    function toonData($email, $pdo) {
+        $sql = "SELECT * FROM gebruiker WHERE email = :email";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+
+    function checkRol($email, $pdo) {
+        $sql = "SELECT rolID FROM gebruiker WHERE email = :email";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+        if ($row) {
+            $rolID = $row['rolID'];
+            switch ($rolID) {
+                case 1:
+                    return "Gebruiker";
+                case 2:
+                    return "Admin";
+                default:
+                    return "Geen rol";
+            }
+        }
+    
+        return "Geen rol gevonden";
+    }
+
+    $userData = toonData($email, $pdo);
+    if ($userData) {
+        $rol = checkRol($email, $pdo);
+        echo "Inloggegevens zijn correct" . "<br>";
+        echo "Voornaam: " . $userData['voornaam'] . "<br>";
+        echo "Achternaam: " . $userData['achternaam'] . "<br>";
+        echo "Email: " . $userData['email'] . "<br>";
+        echo "Rol: " . $rol . "<br>";
+
+        if ($rol == "Admin") {
+            echo '<body style="background-color: lightblue;">';
+        } elseif ($rol == "Gebruiker") {
+            echo '<body style="background-color: lightgreen;">';
+        }
+    }
+} else {
+    
+    header("Location: index.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registratie</title>
+    <title>Gegevens</title>
 </head>
 <body>
-    <h1>Registratie</h1>
-    <form method="POST" action="">
-        <label for="voornaam">Voornaam:</label>
-        <input type="text" id="voornaam" name="voornaam" required><br>
+   
 
-        <label for="achternaam">Achternaam:</label>
-        <input type="text" id="achternaam" name="achternaam" required><br>
-
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email" required><br>
-
-        <label for="password">Wachtwoord:</label>
-        <input type="password" id="password" name="password" required><br>
-
-        <label for="klas">Klas:</label>
-        <input type="text" id="klas" name="klas" required><br>
-
-        <input type="submit" value="Registreren">
-    </form>
 </body>
 </html>
